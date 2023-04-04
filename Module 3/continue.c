507c23515bf1c8fef897f388ddfb7c618e4e1146
Tutorials-Git <arsiddik7615@gmail.com>
2023-04-05 03:14:12 +0600

    After changes

---
+++ Module 3/continue.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for (i=1;i<=10;i=i+1)
+    {
+
+        if (i==5)
+        {
+         continue;
+                    /* code */
+        }
+       printf("%d\n",i);
+
+        
+        /* code */
+    }
+    
+
+    return 0;
+}
